505c7857d11c5c0a7a47ead97b454ce9a17eb558
Seab <honeyboyseab@gmail.com>
2017-04-18 00:24:36 -0400

    set up app to work with Firebase

--- Grocr/Grocr/AppDelegate.swift
+++ Grocr/Grocr/AppDelegate.swift
@@ -21,13 +21,15 @@
  */
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
   
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]? = [:]) -> Bool {
+  private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]? = [:]) -> Bool {
+    FIRApp.configure()
     UIApplication.shared.statusBarStyle = .lightContent
     return true
   }
